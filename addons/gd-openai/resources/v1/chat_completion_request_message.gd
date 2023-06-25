b7a906f9f8998ef1fbbd0eea83a00905bddaf680
Clemens Tolboom <clemens@build2be.com>
2023-06-25 17:47:39 +0200

    Restructure chat messages.

---
+++ addons/gd-openai/resources/v1/chat_completion_request_message.gd
@@ -0,0 +1,17 @@
+class_name ChatCompletionRequestMessage extends Resource
+
+
+## The role of the messages author. One of `system`, `user`, `assistant`, or `function`.[br]
+##[br]
+## Note: function is not supported yet.
+## @required
+@export_enum("system", "user", "assistant", "function") var role:String = "user"
+
+## The contents of the message. `content` is required for all messages except assistant messages with function calls.
+@export var content:String = ""
+
+## The name of the author of this message. `name` is required if role is `function`, and it should be the name of the function whose response is in the `content`. May contain a-z, A-Z, 0-9, and underscores, with a maximum length of 64 characters.
+@export var name:String
+
+## Not supported yet.
+var function_call = null
--- addons/gd-openai/resources/v1/chat_completions_request.gd
+++ addons/gd-openai/resources/v1/chat_completions_request.gd
@@ -16,32 +16,8 @@ const GroupId:String = "gpt"
 ## See ModelsRequest and ModelsResponse for list of sub models
 @export var model:String = "gpt-3.5-turbo"
 
+@export var messages:Array[ChatCompletionRequestMessage] = []
 
-# The role of the messages author. One of system, user, assistant, or function.
-
-## System prompt[br]
-##[br]
-## You can prepare the context for your chat.[br]
-## @required
-@export_multiline var system:String = ""
-
-## Assistant prompt[br]
-##[br]
-## This is useful for continuating previous responses.[br]
-## @optional
-@export_multiline var assistant:String = ""
-
-## User prompt [br]
-##[br]
-## Tell your story to get a continuation for.[br]
-## @required
-@export_multiline var user:String = ""
-
-## Not yet implemented[br]
-##[br]
-## Documentation is currently incomplete.
-## @see https://openai.com/blog/function-calling-and-other-api-updates
-var function:String = ""
 
 func _init():
 	super._init()
@@ -52,13 +28,14 @@ func add_message(d:Dictionary, message_type:StringName, content:String):
 	if not d['body'].has('messages'):
 		d["body"]["messages"] = []
 	d["body"]["messages"].push_back({"role": message_type, "content": content})
-	
+
+
 func build_request(root:String) -> Dictionary:
 	var res:Dictionary = super.build_request(root)
 	res["body"]["model"] = model
-	add_message(res, "system", system)
-	if not assistant == "":
-		add_message(res, "assistant", assistant)
-	add_message(res, "user", user)
+
+	for m in messages:
+		add_message(res, m.role, m.content)
+
 	return res
 
